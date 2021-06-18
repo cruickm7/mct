ecc71050a798bef1e74da57beb0346879d0cdcf4
cruickm7 <60183191+cruickm7@users.noreply.github.com>
2021-06-18 20:55:51 -0300

    8

--- womp.lua
+++ womp.lua
@@ -6,12 +6,13 @@ turtle.up()
 for i = y, 1, -1 do
     for k = 4, 1, -1 do
         for j = i*2, 1, -1 do
-            if turtle.getItemCount(x) == 0 then
+            if turtle.getItemCount(x) <= 1 then
                 x = x + 1
                 turtle.select(x)
             end
             turtle.forward()
             if j ~= 1 then 
+                check()
                 turtle.placeDown()
                 z = z + 1
             end
@@ -19,6 +20,7 @@ for i = y, 1, -1 do
         turtle.turnRight()
         if i ~= y and k ~= 1 then
             turtle.down()
+            check()
             turtle.placeDown()
             turtle.up()
             z = z + 1
@@ -28,9 +30,31 @@ for i = y, 1, -1 do
     turtle.turnRight()
     turtle.forward()
     turtle.turnLeft()
+    check()
     turtle.placeDown()
     turtle.up()
 end
 turtle.placeDown()
 z = z + 1
 print("Used " .. z  .. "blocks")
+
+function check()
+    q = 0
+    for h = 1, 16, 1 do
+        if turtle.getItemCount(h) <= 1 then
+            q = q + 1
+        end
+    end
+    if q == 16 then
+        while ~turtle.suck() do
+            turtle.suck()
+        end
+    end
+    if turtle.getItemCount(x) <= 1 then
+        x = x + 1
+        turtle.select(x)
+        if x == 16 then
+            x = 1
+        end
+    end
+end
