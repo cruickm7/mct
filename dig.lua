31a2c5b191eed586ced0371bc2fdec4d0af455bd
cruickm7 <60183191+cruickm7@users.noreply.github.com>
2021-06-18 19:20:25 -0300

    4

--- dig.lua
+++ dig.lua
@@ -5,15 +5,13 @@ for j = 1, 4, 1 do
         turtle.placeDown()
         turtle.forward()
     end
-    turtle.back()
     for k = 0, d, 1 do
         turtle.forward()
         turtle.turnRight()
         turtle.forward()
         if k ~= d then
             turtle.placeDown()
+            turtle.turnLeft() 
         end
-        turtle.turnLeft()
     end
-    turtle.turnRight()
 end
