a0e50dd7e05b847749fac87efd0915f2b3802b23
cruickm7 <60183191+cruickm7@users.noreply.github.com>
2021-06-19 02:29:16 -0300

    26

--- add.lua
+++ add.lua
@@ -1,8 +1,21 @@
 rednet.open("right")
 turtle.up()
-for i = 1, 16, 1 do
-    if turtle.getItemCount() ~= 0 then
-        print(turtle.getItemDetail.name)
+
+function search(s)
+    for i = 1, 16, 1 do
+        print(i)
+        turtle.select(i)
+        if turtle.getItemDetail().name == s then
+            break
+        end
     end
 end
+
+search("minecraft:lava_bucket")
+turtle.placeDown()
+turtle.up()
+search("minecraft:cobblestone")
+while rednet.receive() ~= "go" do
+end
+search("minecraft:water_bucket")
 turtle.placeDown()
