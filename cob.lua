d68c1856ddd64b47f4d01c99cb4fe554119d4f84
cruickm7 <60183191+cruickm7@users.noreply.github.com>
2021-06-23 22:20:26 -0300

    36

--- cob.lua
+++ cob.lua
@@ -2,6 +2,8 @@ t = 0
 while true do
     if turtle.inspect() then
         turtle.dig()
+        t = t + 1
+        print(t)
     end
     if turtle.getItemCount() == 64 then
         turtle.dropDown()
